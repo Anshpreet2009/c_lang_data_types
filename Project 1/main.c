b6f89ae140fbb63f89cb8407a8969d5baa08243d
Anshpreet2009 <109532905+Anshpreet2009@users.noreply.github.com>
2026-01-05 12:04:07 +0530

    Add files via upload

---
+++ Project 1/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>   //include standard input output library header
+#include <stdbool.h> //include standard boolean library header
+
+int main() // main function of the program
+{
+    char symbol1 = '!'; // this data type holds a single character
+    char symbol2 = '@';
+    char symbol3 = '#';
+
+    char name1[] = "Papyrus"; // this data type holds an array of characters (string)
+    char name2[] = "Sans";
+    char name3[] = "Undyne";
+
+    int int1 = 18; // this data type holds whole numbers
+    int int2 = 30;
+    int int3 = -25;
+
+    float dec1 = 3.14; // this data type holds decimal numbers with single precision
+    float dec2 = 2.71;
+    float dec3 = 1.5;
+
+    double precise1 = 3.141592653589793; // this data type holds decimal numbers with double the precision of a float
+    double precise2 = 2.718281828459045;
+    double precise3 = 1.618033988749895;
+
+    bool in_college = false; // this data type holds a true value (boolean)
+    bool employed = 1;       // booleans can also be represented as integers (0 for false, non-zero for true)
+    bool in_school = true;   // this data type holds a false value (boolean)
+
+    printf("symbols: \n");
+    printf("%c\n", symbol1); // print the symbol and go to a new line
+    printf("%c\n", symbol2); // print the symbol and go to a new line
+    printf("%c\n", symbol3); // print the symbol and go to a new line
+
+    printf("names: \n");
+    printf("%s\n", name1); // print the string
+    printf("%s\n", name2); // print the string
+    printf("%s\n", name3); // print the string
+
+    printf("integers: \n");
+    printf("%3d\n", int1);   // print the integer with a minimum width of 3(minimum number of characters to be printed)
+    printf("%-4d\n", int2);  // print the integer with a minimum width of 4, left-aligned
+    printf("%+05d\n", int3); // print the integer with a minimum width of 5, padded with leading zeros, and always show the sign
+
+    printf("decimals: \n");
+    printf("%.2f\n", dec1);  // print the float with 2 decimal places
+    printf("%.4f\n", dec2);  // print the float with 4 decimal places
+    printf("%7.1f\n", dec3); // print the float with 1 decimal place, minimum width of 7
+
+    printf("precise decimals: \n");
+    printf("%.15lf\n", precise1); // print the double with 15 decimal places
+    printf("%.15lf\n", precise2); // print the double with 15 decimal places
+    printf("%.15lf\n", precise3); // print the double with 15 decimal places
+
+    printf("booleans: \n");
+    printf("%d\n", in_school);  // print the boolean as an integer
+    printf("%d\n", employed);   // print the boolean as an integer
+    printf("%d\n", in_college); // print the boolean as an integer
+
+    return 0;
+}
