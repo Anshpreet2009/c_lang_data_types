7ff02f4487800633b5f9f0cb509550fc2493180f
Anshpreet2009 <109532905+Anshpreet2009@users.noreply.github.com>
2026-01-06 21:39:23 +0530

    Add files via upload

--- Project 1/main.c
+++ Project 1/main.c
@@ -4,19 +4,19 @@
 int main() // main function of the program
 {
     char symbol1 = '!'; // this data type holds a single character
-    char symbol2 = '@';
+    char symbol2 = '@'; // Its value can be set to '\0 to represent the null character
     char symbol3 = '#';
 
     char name1[] = "Papyrus"; // this data type holds an array of characters (string)
-    char name2[] = "Sans";
+    char name2[] = "Sans";    // Its value can be set to "" to represent an empty string
     char name3[] = "Undyne";
 
     int int1 = 18; // this data type holds whole numbers
-    int int2 = 30;
+    int int2 = 30; // Its value can be set to 0 to represent zero
     int int3 = -25;
 
     float dec1 = 3.14; // this data type holds decimal numbers with single precision
-    float dec2 = 2.71;
+    float dec2 = 2.71; // Its value can be set to 0.0f to represent zero
     float dec3 = 1.5;
 
     double precise1 = 3.141592653589793; // this data type holds decimal numbers with double the precision of a float
@@ -29,13 +29,13 @@ int main() // main function of the program
 
     printf("symbols: \n");
     printf("%c\n", symbol1); // print the symbol and go to a new line
-    printf("%c\n", symbol2); // print the symbol and go to a new line
-    printf("%c\n", symbol3); // print the symbol and go to a new line
+    printf("%c\n", symbol2);
+    printf("%c\n", symbol3);
 
     printf("names: \n");
     printf("%s\n", name1); // print the string
-    printf("%s\n", name2); // print the string
-    printf("%s\n", name3); // print the string
+    printf("%s\n", name2);
+    printf("%s\n", name3);
 
     printf("integers: \n");
     printf("%3d\n", int1);   // print the integer with a minimum width of 3(minimum number of characters to be printed)
@@ -49,13 +49,13 @@ int main() // main function of the program
 
     printf("precise decimals: \n");
     printf("%.15lf\n", precise1); // print the double with 15 decimal places
-    printf("%.15lf\n", precise2); // print the double with 15 decimal places
-    printf("%.15lf\n", precise3); // print the double with 15 decimal places
+    printf("%.15lf\n", precise2);
+    printf("%.15lf\n", precise3);
 
     printf("booleans: \n");
-    printf("%d\n", in_school);  // print the boolean as an integer
-    printf("%d\n", employed);   // print the boolean as an integer
-    printf("%d\n", in_college); // print the boolean as an integer
+    printf("%d\n", in_school); // print the boolean as an integer
+    printf("%d\n", employed);
+    printf("%d\n", in_college);
 
     return 0;
 }
